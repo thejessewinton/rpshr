d84ae8ee9e73d69aad517866034004e327ccec4e
Jesse Winton <jrandallwinton@gmail.com>
2024-04-22 00:56:37 -0400

    fixes

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ await import('./src/env.js')
 
 /** @type {import("next").NextConfig} */
 const config = {
+  experimental: {
+    typedRoutes: true
+  },
   images: {
     remotePatterns: [{ hostname: 'lh3.googleusercontent.com', protocol: 'https' }]
   }
