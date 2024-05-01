f0f1939ff8e8dd5c928ea8d44ed9dec6fedae863
Jesse Winton <jrandallwinton@gmail.com>
2024-04-30 21:42:04 -0400

    core working

--- src/server/api/schemas/sets.ts
+++ src/server/api/schemas/sets.ts
@@ -1,13 +1,14 @@
 import { z } from 'zod'
 
-export const workoutRegex = /(\d+)\s+(\d+)\s+(\d+lbs?)(?=(?:, |$))/g
-
-export const exerciseRegex =
+export const workoutRegex =
   /\b(?:Today|Yesterday|\d{1,2}(?:\/\d{1,2}){1,2}(?:\s+)?(?:AD|BC)?(?:,)?|(\d+)\s*(?:(?:\s|x|@)\s*|\s)(\d+)\s+(\d+)(lbs|kgs)?\.?\s*(?:,(.*?))?\s*(?:,(.*?))?)$/
+
+export const setsRegex = /(\d+)\s+(\d+)\s+(\d+)(lbs?|kgs?)(?=(?:, |$))/g
+export const setRegex = /(\d+|\w+)/g
 
 export const dateAndNotesRegex = /\b(?:Today|Yesterday|\d{1,2}(?:[\/-]\d{1,2}){1,2}(?:\s+)?(?:,)?)(?:\s*,\s*)?(.*)$/
 
 export const setSchema = z.object({
-  sets: z.string().regex(workoutRegex),
+  sets: z.string().regex(setsRegex),
   lift_id: z.number()
 })
--- src/server/api/transformers/sets.ts
+++ src/server/api/transformers/sets.ts
@@ -1,21 +1,24 @@
 import { type z } from 'zod'
 
-import { dateAndNotesRegex, exerciseRegex, workoutRegex } from '~/server/api/schemas/sets'
+import { dateAndNotesRegex, setRegex, setsRegex, workoutRegex } from '~/server/api/schemas/sets'
 import { setInsertSchema } from '~/server/db/schema'
 import dayjs, { normalizeDateString } from '~/utils/date'
 
 export const transformSetString = (input: string) => {
   const sets: Omit<z.infer<typeof setInsertSchema>, 'sets' | 'user_id'>[] = []
 
-  const exerciseMatch = input.match(exerciseRegex)
+  const exerciseMatch = input.match(workoutRegex)
 
   if (exerciseMatch) {
-    const exercises = exerciseMatch[0].match(workoutRegex)
-    const dateAndNotes = exerciseMatch[0].match(dateAndNotesRegex)
+    const [firstMatch] = exerciseMatch
+
+    const exercises = firstMatch.match(setsRegex)
+    const dateAndNotes = firstMatch.match(dateAndNotesRegex)
+
     if (!exercises) return sets
 
     exercises.forEach((exercise) => {
-      const [numberOfSets, numberOfReps, weight] = exercise.split(/\s+/)
+      const [numberOfSets, numberOfReps, weight, unit] = exercise.match(setRegex)!
 
       const [dateString, notes] = dateAndNotes![0].split(/,\s*/)
       let date: Date = dayjs().toDate()
@@ -25,10 +28,11 @@ export const transformSetString = (input: string) => {
       const data = setInsertSchema.omit({ user_id: true }).parse({
         reps: parseInt(numberOfReps!),
         weight: parseInt(weight!),
-        sets: parseInt(numberOfSets!)
+        sets: parseInt(numberOfSets),
+        unit
       })
 
-      Array.from({ length: parseInt(numberOfSets!) }).forEach(() => {
+      Array.from({ length: parseInt(numberOfSets) }).forEach(() => {
         sets.push({ reps: data.reps, weight: data.weight, unit: data.unit ?? 'lbs', notes: notes?.trim(), date })
       })
     })
@@ -36,5 +40,3 @@ export const transformSetString = (input: string) => {
 
   return sets
 }
-
-// 5 5 135lbs, 4 5 145lbs, 3 5 155lbs, Today, Felt great
