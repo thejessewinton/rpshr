212340c00d8e6a7c53532cc5ff414d9026601da3
Jesse Winton <jrandallwinton@gmail.com>
2024-05-05 20:40:19 -0400

    updates

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -7,10 +7,7 @@ import { Logo } from '~/components/shared/logo'
 export const Header = () => {
   return (
     <header className='flex items-center justify-between px-8 py-4'>
-      <Link
-        href='/'
-        className='rounded-lg p-2 outline-none transition-colors hover:bg-neutral-200/70 focus:bg-neutral-200/70 hover:dark:bg-neutral-700/20 focus:dark:bg-neutral-700/20'
-      >
+      <Link href='/'>
         <Logo className='size-6' />
       </Link>
 
