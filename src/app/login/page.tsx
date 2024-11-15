d85fd66a014ec41f81cf7e0b76863f19caff690b
Jesse Winton <jesse@appwrite.io>
2024-11-15 11:58:01 -0500

    login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,7 @@ import { GoogleLogo } from '@phosphor-icons/react/dist/ssr'
 
 import { Button } from '~/components/shared/button'
 import { Logo } from '~/components/shared/logo'
+import { LoginBg } from '~/components/ui/login-bg'
 import { auth, signIn } from '~/server/auth'
 
 export const metadata: Metadata = {
@@ -46,38 +47,49 @@ export default async function SignIn({
   }
 
   return (
-    <div className="grid h-screen grid-cols-12 items-center justify-center dark:bg-neutral-900">
-      <div className="col-span-4 flex h-full flex-col items-center justify-center bg-neutral-950">
-        <h2 className="animate-enter font-medium text-neutral-950 dark:text-white">
-          Breathe.
-        </h2>
-        <h2 className="animate-enter font-medium text-neutral-950 [animation-delay:750ms] dark:text-white">
-          Focus.
-        </h2>
-        <h2 className="animate-enter font-medium text-neutral-950 [animation-delay:1500ms] dark:text-white">
-          Write.
-        </h2>
-      </div>
-      <div className="col-span-8 w-full max-w-2xl space-y-6 text-center">
-        <Logo className="mx-auto size-8" />
-        <div className="flex w-full items-center justify-center gap-2">
-          {providers.map((provider) => (
-            <form action={provider.action} key={provider.name}>
-              <Button
-                className="w-full py-2"
-                icon={<provider.icon className="size-4 text-neutral-400" />}
-              >
-                Continue with {provider.name}
-              </Button>
-            </form>
-          ))}
-        </div>
-        {error && (
-          <div className="max-auto font-light text-gray-700 text-sm dark:text-gray-400">
-            {errorMap[error]}
+    <>
+      <div className="flex h-screen items-center justify-center">
+        <div className="w-full max-w-2xl space-y-6 text-center">
+          <Logo className="relative z-10 mx-auto size-8" />
+          <div className="relative z-10 flex w-full items-center justify-center gap-2">
+            {providers.map((provider) => (
+              <form action={provider.action} key={provider.name}>
+                <Button
+                  className="w-full py-2"
+                  icon={<provider.icon className="size-4 text-neutral-400" />}
+                >
+                  Continue with {provider.name}
+                </Button>
+              </form>
+            ))}
           </div>
-        )}
+          {error && (
+            <div className="max-auto font-light text-gray-700 text-sm dark:text-gray-400">
+              {errorMap[error]}
+            </div>
+          )}
+          <div className="!mt-0 absolute inset-0 z-0 bg-neutral-900/50 backdrop-blur-sm [mask-image:linear-gradient(to_bottom,rgba(0,0,0,1)_0%,rgba(0,0,0,1)_30%,transparent_50%,rgba(0,0,0,1)_70%)]" />
+        </div>
       </div>
-    </div>
+      <LoginBg />
+    </>
   )
 }
+
+//  backdrop-filter: blur(6px);
+//                 background-color: hsl(var(--web-color-background) / 50%);
+//                 mask-composite: intersect;
+//                 mask-image: linear-gradient(
+//                         to top,
+//                         transparent,
+//                         rgba(0, 0, 0, 1) 25%,
+//                         rgba(0, 0, 0, 1) 75%,
+//                         transparent
+//                     ),
+//                     linear-gradient(
+//                         to right,
+//                         transparent,
+//                         rgba(0, 0, 0, 1) 25%,
+//                         rgba(0, 0, 0, 1) 75%,
+//                         transparent
+//                     );
