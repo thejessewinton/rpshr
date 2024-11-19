6a77d34d161e6792c656edb5f440924c2c830263
Jesse Winton <jesse@appwrite.io>
2024-11-19 14:38:40 -0500

    update header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -3,7 +3,6 @@
 import Link from 'next/link'
 
 import { Logo } from '~/components/shared/logo'
-import { Button } from '../shared/button'
 
 export const Header = () => {
   return (
@@ -11,13 +10,6 @@ export const Header = () => {
       <Link href="/" className="rounded-full outline-none focus:ring-2">
         <Logo />
       </Link>
-
-      <Button
-        href="/login"
-        className="animate-enter text-sm [animation-delay:7000ms]"
-      >
-        Start now
-      </Button>
     </header>
   )
 }
