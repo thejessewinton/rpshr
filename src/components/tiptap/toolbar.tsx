790df41a7c58a792704557824391c70deb1051ea
Jesse Winton <jesse@appwrite.io>
2024-12-12 10:38:41 -0500

    tweaks

--- src/components/tiptap/toolbar.tsx
+++ src/components/tiptap/toolbar.tsx
@@ -50,49 +50,51 @@ export const Toolbar = ({
   }
 
   return (
-    <MotionConfig
-      transition={{
-        duration: 0.1,
-        repeatDelay: 4,
-        type: 'spring',
-        damping: 10,
-      }}
-    >
-      <AnimatePresence>
-        <motion.div
-          className="fixed bottom-10 left-1/2 flex origin-center items-center rounded-full border border-neutral-300/40 bg-white py-1.5 pr-2 pl-4 shadow-black/10 shadow-xl dark:border-neutral-700/40 dark:bg-neutral-900"
-          initial={{
-            opacity: 0,
-            translateY: 10,
-            filter: 'blur(4px)',
-            translateX: '-50%',
-          }}
-          animate={{
-            opacity: 1,
-            translateY: 0,
-            filter: 'blur(0px)',
-          }}
-          whileHover={{
-            scale: 1,
-          }}
-        >
-          <div className="relative flex items-center justify-center gap-1">
-            <SaveState
-              isPending={isPending}
-              isSuccess={isSuccess}
-              isError={isError}
-            />
-            <WordCount />
-            <Notes />
-            <NewButton />
-            {noteId && <DeleteButton noteId={noteId} />}
-            <FocusSwitcher />
-            <ThemeSwitcher />
-            <SignOutButton />
-          </div>
-        </motion.div>
-      </AnimatePresence>
-    </MotionConfig>
+    <Tooltip.Provider delayDuration={50} skipDelayDuration={1000}>
+      <MotionConfig
+        transition={{
+          duration: 0.1,
+          repeatDelay: 4,
+          type: 'spring',
+          damping: 10,
+        }}
+      >
+        <AnimatePresence>
+          <motion.div
+            className="fixed bottom-10 left-1/2 flex origin-center items-center rounded-full border border-neutral-300/40 bg-white py-1.5 pr-2 pl-4 shadow-black/10 shadow-xl dark:border-neutral-700/40 dark:bg-neutral-900"
+            initial={{
+              opacity: 0,
+              translateY: 10,
+              filter: 'blur(4px)',
+              translateX: '-50%',
+            }}
+            animate={{
+              opacity: 1,
+              translateY: 0,
+              filter: 'blur(0px)',
+            }}
+            whileHover={{
+              scale: 1,
+            }}
+          >
+            <div className="relative flex items-center justify-center gap-1">
+              <SaveState
+                isPending={isPending}
+                isSuccess={isSuccess}
+                isError={isError}
+              />
+              <WordCount />
+              <Notes />
+              <NewButton />
+              {noteId && <DeleteButton noteId={noteId} />}
+              <FocusSwitcher />
+              <ThemeSwitcher />
+              <SignOutButton />
+            </div>
+          </motion.div>
+        </AnimatePresence>
+      </MotionConfig>
+    </Tooltip.Provider>
   )
 }
 
