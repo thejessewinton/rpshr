5f7bb4703c7e2ada9e5bd9d0bd9fc09a6893e941
Jesse Winton <jesse@appwrite.io>
2025-08-22 11:48:10 -0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,23 @@ import '~/styles/globals.css'
 import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
 
-import { GeistMono } from 'geist/font/mono'
-import { GeistSans } from 'geist/font/sans'
+import { Geist, Geist_Mono } from 'next/font/google'
 
 import { ThemeProvider } from 'next-themes'
 import { env } from '~/env'
 import { Hotkeys } from '~/providers/hotkeys'
 import { TRPCReactProvider } from '~/trpc/react'
 import { cn } from '~/utils/cn'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: {
@@ -29,8 +38,8 @@ export default async function RootLayout({
       <body
         className={cn(
           'bg-neutral-50 font-sans antialiased selection:bg-blue-500 selection:text-white dark:bg-neutral-900 dark:selection:bg-blue-950',
-          GeistSans.variable,
-          GeistMono.variable,
+          geistSans.variable,
+          geistMono.variable,
         )}
       >
         <ThemeProvider attribute="class" disableTransitionOnChange>
