72261b3ac40866852c1f4f8012142205632a4605
Jesse Winton <jesse@cosmos.so>
2025-11-22 12:35:26 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ import type { NextConfig } from 'next'
 const config = {
   reactCompiler: true,
   typedRoutes: true,
-  cacheComponents: true,
   experimental: {
     useCache: true,
     turbopackFileSystemCacheForDev: true,
